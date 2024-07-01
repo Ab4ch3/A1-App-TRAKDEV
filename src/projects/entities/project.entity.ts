d325730c584af6653b6b13b06acf26f5e9410048
Miguel Abache <miiguel.abache@gmail.com>
2024-07-01 13:05:46 -0400

    Entities

--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,1 +1,10 @@
-export class Project {}
+import { IProject } from 'src/interfaces/project.interface';
+import { BaseEntity, Column, Entity } from 'typeorm';
+
+@Entity({ name: 'projects' })
+export class ProjectEntity extends BaseEntity implements IProject {
+  @Column()
+  name: string;
+  @Column()
+  description: string;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class UsersEntity {}
+import { IUser } from 'src/interfaces/user.interface';
+import { Column, Entity, BaseEntity } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class UsersEntity extends BaseEntity implements IUser {
+  @Column()
+  firstName: string;
+  @Column()
+  lastName: string;
+  @Column()
+  age: number;
+  @Column()
+  email: string;
+  @Column()
+  username: string;
+  @Column()
+  password: string;
+  @Column()
+  role: string;
+}
